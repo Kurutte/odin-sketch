7e21801955c816ff92e3f179579abafa60b8f16e
Kurutte <eelleeDev@gmail.com>
2024-10-01 14:15:29 -0700

    Fully Implemented Functionality
    Site has now fully implemented functionality from Odin's project requirements, including the extra credit options (Implemented opacity changing to more similarly mimic an etch-a-sketch, and a way to change the mode of the "pen" from black only to random colors. Also implemented notifications to inform the user of their current grid size and color setting.

--- logic.js
+++ logic.js
@@ -1,9 +1,17 @@
 const container = document.querySelector(".container");
-const reset = document.querySelector("button");
+const colorNotif = document.querySelector("#colorNotif");
+const sizeNotif = document.querySelector("#sizeNotif");
+const reset = document.querySelector("#reset");
 reset.addEventListener('click', resetBoard);
+const colors = document.querySelector("#colors");
+colors.addEventListener('click', useRandomColors);
+const onlyBlack = document.querySelector("#black");
+onlyBlack.addEventListener('click', useBlackOnly);
+let applyRandomColors = false;
+let gridSize = 16;
 
 function resetBoard() {
-    let gridSize = parseInt(prompt("How big do you want your new grid?", "1-100"));
+    gridSize = parseInt(prompt("How big do you want your new grid?", "1-100"));
     while(gridSize > 100 || gridSize < 0 || !Number.isInteger(gridSize)) {
         gridSize = parseInt(prompt("Not a valid size. Please only use Integers and a number 1-100", "1-100"));
     }
@@ -11,10 +19,54 @@ function resetBoard() {
     const grid = document.querySelector(".grid");
     container.removeChild(grid);
     createGrid(gridSize);
+    updateSizeNotif();
 }
 
 function colorSquare() {
-    this.style.backgroundColor = "black";
+    let newOpacity = parseFloat(this.style.opacity);
+    if(newOpacity < 1) {
+        newOpacity += .1;
+    }
+    this.style.backgroundColor = getRandomColor();
+    this.style.opacity = "" + newOpacity + "";
+}
+
+function getRandomColor() {
+    let red = 0, green = 0, blue = 0;
+
+    if(applyRandomColors) {
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+    }
+
+    let colorString = "rgb(" + red + " " + green + " " + blue + ")";
+    return colorString;
+}
+
+function useRandomColors() {
+    applyRandomColors = true;
+    alert("Noted! Random Colors Here We GO!");
+    updateColorNotif();
+}
+
+function useBlackOnly() {
+    applyRandomColors = false;
+    alert("Noted! Only Black From Now On!");
+    updateColorNotif();
+}
+
+function updateColorNotif() {
+    if(applyRandomColors) {
+        colorNotif.textContent = "Current Color Setting: Random Colors";
+    }
+    else {
+        colorNotif.textContent = "Current Color Setting: Black Only";
+    }
+}
+
+function updateSizeNotif() {
+    sizeNotif.textContent = "Current size: " + gridSize + " x " + gridSize;
 }
 
 function createGrid(gridSize) {
@@ -27,6 +79,7 @@ function createGrid(gridSize) {
 
         for(rowIndex = 0; rowIndex < gridSize; rowIndex++) {
             const newDiv = document.createElement("div");
+            newDiv.style.opacity = "0";
             newDiv.addEventListener('mouseenter', colorSquare);
             row.appendChild(newDiv);
         }
@@ -37,5 +90,7 @@ function createGrid(gridSize) {
     container.appendChild(grid);
 }
 
-createGrid(16);
+createGrid(gridSize);
+updateColorNotif();
+updateSizeNotif();
 
