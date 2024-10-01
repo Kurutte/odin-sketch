624d78abbf8400fd09c72b64b49abff95cb141e1
Kurutte <eelleeDev@gmail.com>
2024-10-01 13:31:42 -0700

    Completed Basic Functionality of Full Etch-A-Sketch Project
    Steps 3 and 4 completed, allowing for the grid to color in as you enter squares with your mouse, and a button to reset the grid and change the size from 1-100 (including safeguards for prompts outside that range). Grid also stays at 960px regardless of grid size.

--- logic.js
+++ logic.js
@@ -1,17 +1,41 @@
 const container = document.querySelector(".container");
+const reset = document.querySelector("button");
+reset.addEventListener('click', resetBoard);
 
-const grid = document.querySelector(".grid");
-
-for(index = 0; index < 16; index++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-
-    for(rowIndex = 0; rowIndex < 16; rowIndex++) {
-        const newDiv = document.createElement("div");
-        row.appendChild(newDiv);
+function resetBoard() {
+    let gridSize = parseInt(prompt("How big do you want your new grid?", "1-100"));
+    while(gridSize > 100 || gridSize < 0 || !Number.isInteger(gridSize)) {
+        gridSize = parseInt(prompt("Not a valid size. Please only use Integers and a number 1-100", "1-100"));
     }
 
-    grid.appendChild(row);
+    const grid = document.querySelector(".grid");
+    container.removeChild(grid);
+    createGrid(gridSize);
 }
 
-container.appendChild(grid);
+function colorSquare() {
+    this.style.backgroundColor = "black";
+}
+
+function createGrid(gridSize) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+
+    for(index = 0; index < gridSize; index++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        for(rowIndex = 0; rowIndex < gridSize; rowIndex++) {
+            const newDiv = document.createElement("div");
+            newDiv.addEventListener('mouseenter', colorSquare);
+            row.appendChild(newDiv);
+        }
+
+        grid.appendChild(row);
+    }
+
+    container.appendChild(grid);
+}
+
+createGrid(16);
+
